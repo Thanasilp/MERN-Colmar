60bf4db9ccb955e3f97a53cfb40decec96e5ec79
Thanasilp <thanasin.pin@gmail.com>
2024-12-12 13:33:02 +0700

    react deleted

--- src/pages/Campus.tsx
+++ src/pages/Campus.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export const Campus = () => {
   return <div>OnCampus</div>;
 };
--- src/pages/Course.tsx
+++ src/pages/Course.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export const Course = () => {
   return <div>Course</div>;
 };
